55f07daebadc93e5a0088d62ca7fb824dd41b73b
TovarasulTony <tonyfrum@yahoo.com>
2019-04-03 14:12:44 +0300

    Added something for demo

--- Presentations/Bash/demo_1.sh
+++ Presentations/Bash/demo_1.sh
@@ -1,5 +1,14 @@
 #source: https://www.youtube.com/watch?v=7uwW20odwEk
+***Keywords
+Pipe
+Variable
+Command
+Grep Global/RegularExpresion/print
+Methods
+${}
+$()
 
+***Draft
 mkdir 00 01 02
 ls | cut -d. -f1 |less
 ls | cut -d. -f1 | uniq
@@ -32,3 +41,54 @@ for f in *(.);
  do print $f;
  done
  
+***Steps:
+
+
+
+**|Organizing Photos
+
+The images format:
+
+YEAR.MONTH.NAME.CATEGORY.png
+
+*>Making directories
+   {
+    Playing with cut:
+    cut -d, f 2,4 oscar_felame_winners.csv
+
+    - vb de :
+    >-d poate si de c
+    >linii
+    >f
+   }
+
+   -decide grouping rule
+
+   YEAR:
+   ls | cut -d . -f 1 | uniq
+   MONTH:
+   ls | cut -d . -f 2 | sort | uniq
+   CATEGORY:
+   ls | rev | cut -d . -f 2 | rev | sort | uniq
+
+
+*>Moving Pictures
+-decide image's category from it's name
+-do this with all the images
+-move them
+
+for f in *(.)
+do
+ #do stuff here
+done
+
+for f in *(.); do d=?; mv $f $d;done
+
+YEAR:
+ d=${f%%.*}
+MONTH:
+ d=${f#*.} d=${d%%.*}
+CATEGORY:
+ d=${f%.*} d=${d##*.}
+
+for f in *(.); do d=${f%.*} d=${d##*.}; mv $f $d;done
