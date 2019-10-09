58445d1eefb0d702098be74863bfea98c05b7ee9
Tovarasul <tonyfrum@yahoo.com>
2019-10-09 16:08:23 +0300

    added wotking with timers

---
+++ Learn C#/Exercise 1 - Work with timers/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Timers;
+
+namespace SignalTime
+{
+    class Program
+    {
+        static void Main()
+        {
+            Timer timer = new Timer(200);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            while (true)
+            {
+                // Infinite loop.
+            }
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // Use SignalTime.
+            DateTime time = e.SignalTime;
+            Console.WriteLine("TIME: " + time);
+        }
+    }
+}
---
+++ Learn C#/Exercise 1 - Work with timers/DateList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace DateList
+{
+    class Program
+    {
+        static Timer _timer;
+        static List<DateTime> _l;
+
+        public static List<DateTime> DateList
+        {
+            get
+            {
+                if (_l == null) // Lazily initialize the timer.
+                {
+                    Start(); // Start the timer.
+                }
+                return _l; // Return the list of dates.
+            }
+        }
+
+        static void Start()
+        {
+            _l = new List<DateTime>(); // Allocate the list.
+            _timer = new Timer(3000); // Set up the timer for 3 seconds.
+            _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+            _timer.Enabled = true;
+        }
+
+        static void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _l.Add(DateTime.Now); // Add date on each timer event.
+        }
+    }
+}
+}
