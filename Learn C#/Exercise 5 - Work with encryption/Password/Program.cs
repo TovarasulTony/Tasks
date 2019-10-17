faa57e18f16268d322b52abd745e795403645fac
Tovarasul <tonyfrum@yahoo.com>
2019-10-17 17:25:34 +0300

    added exercise 5

---
+++ Learn C#/Exercise 5 - Work with encryption/Password/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            Aes myAes = Aes.Create();
+
+            Console.WriteLine("Please insert the password that you are using for every online account(yeah... you do that):");
+            password = Console.ReadLine();
+
+            Console.WriteLine(password);
+
+            Aes aesAlg = Aes.Create();
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(myAes.Key, myAes.IV);
+            MemoryStream msEncrypt = new MemoryStream();
+            CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            StreamWriter swEncrypt = new StreamWriter(csEncrypt);
+
+            swEncrypt.Write(password);
+            var encrypted = msEncrypt.ToArray();
+
+            //----
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(myAes.Key, myAes.IV);
+            MemoryStream msDecrypt = new MemoryStream(encrypted);
+            CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            StreamReader srDecrypt = new StreamReader(csDecrypt);
+
+            var decrypted = srDecrypt.ReadToEnd();
+
+            Console.WriteLine(decrypted);
+
+            Console.ReadLine();
+        }
+    }
+}
